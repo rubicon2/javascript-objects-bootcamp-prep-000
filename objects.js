824906824b680b90578d070493cc0b8b8a851ee0
Liam Argent <liargent@hotmail.com>
2019-11-07 21:23:58 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -9,7 +9,7 @@ console.log("Original: " + playlist["Angels of Light"]);
 console.log("     New: " + newList["Angels of Light"]);
 
 removeFromPlaylist(newList, "Radiohead");
-console.log(newList["Radiohead"]); 
+console.log(newList); 
 
 function updatePlaylist(playlistObject, artistName, songName) {
   playlistObject[artistName] = songName;
