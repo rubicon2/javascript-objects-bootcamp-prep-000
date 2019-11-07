b90f63f9d394b5627f2f4bc201124c321ed3f67d
Liam Argent <liargent@hotmail.com>
2019-11-07 21:22:54 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,9 +1,22 @@
 /* playlist can only store 1 song per artist as cannot store duplicate keys */
 
+var playlist = new Object({["Deerhunter"]: "Nothing Ever Happened"}); 
+
+updatePlaylist(playlist, "Radiohead", "Idioteque");
+var newList = updatePlaylist(playlist, "Angels of Light", "Not Here/Not Now");
+
+console.log("Original: " + playlist["Angels of Light"]);
+console.log("     New: " + newList["Angels of Light"]);
+
+removeFromPlaylist(newList, "Radiohead");
+console.log(newList["Radiohead"]); 
+
 function updatePlaylist(playlistObject, artistName, songName) {
-  return playlist.assign({}, playlistName, { artistName : songName });
+  playlistObject[artistName] = songName;
+  return playlistObject;
 }
 
 function removeFromPlaylist(playlistObject, artistName) {
   delete playlistObject.artistName; 
+  return playlistObject; 
 }
